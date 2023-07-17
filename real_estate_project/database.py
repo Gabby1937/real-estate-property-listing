ef76dc51b64d11b73349853ee8e26edf5a594dad
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-07-17 14:59:01 +0100

    change database.py secrect key to Auth0 client-key.

--- real_estate_project/database.py
+++ real_estate_project/database.py
@@ -10,7 +10,7 @@ UPLOAD_FOLDER = 'static/images'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://boss:key@localhost:5432/realestatedb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = 'mysecretkey'
+app.secret_key = 'MVyH6mODK2CMHCvCGfVHXzxPe9E2Pmnap9tTzXabDttc1tOLY3f9Z3oYam-5PEPT'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
